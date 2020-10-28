20ecbfa753de464a09388e203f8362c42faaaf5c
Vitor da Silva <vitords.42@gmail.com>
2020-10-28 11:25:52 +0100

    Change x to xs to please the variable gods

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
 
-fn bogosort<T: Ord>(x: &mut [T]) {
-    fn is_sorted<T: Ord>(x: &[T]) -> bool {
-        for window in x.windows(2) {
+fn bogosort<T: Ord>(xs: &mut [T]) {
+    fn is_sorted<T: Ord>(xs: &[T]) -> bool {
+        for window in xs.windows(2) {
             if window[0] > window[1] {
                 return false;
             }
@@ -13,15 +13,16 @@ fn bogosort<T: Ord>(x: &mut [T]) {
 
     let mut rng = thread_rng();
 
-    while !is_sorted(x) {
-        x.shuffle(&mut rng);
+    while !is_sorted(xs) {
+        xs.shuffle(&mut rng);
     }
 }
 
-fn main() {
-    let mut x = vec![1i32, 9, 4, 7, 5];
 
-    println!("{:?}", x);
-    bogosort(&mut x);
-    println!("{:?}", x);
+fn main() {
+    let mut xs = vec![1i32, 9, 4, 7, 5];
+    println!("{:?}", xs);
+
+    bogosort(&mut xs);
+    println!("{:?}", xs);
 }
